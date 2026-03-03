6f2343db073fc71dd01443c5f7dfca58c4465120
KASHISHROY <kashishroy9903@gmail.com>
2026-03-03 18:22:23 +0530

    Initial commit: Social media app with posts, tags and reactions

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,55 @@
+import { createContext,useReducer } from "react";
+
+
+export const PostList=createContext({
+    postList:[],
+    addPost:()=>{},
+    deletePost:()=>{},
+});
+const postListReducer=(currPostList,action)=>{
+return currPostList;
+}
+
+const PostListProvider=({children})=>{
+   const[postList,dispatchPostList]=useReducer(postListReducer,DEFAULT_POST_LIST); 
+   
+const addPost=()=>{
+
+    }
+    const deletePost=()=>{
+
+    }
+
+
+return <PostList.Provider value={{
+    postList:postList,
+    addPost:addPost,
+    deletePost:deletePost
+}}>
+    {children}
+</PostList.Provider>;
+};
+
+
+const DEFAULT_POST_LIST=[{
+    id:'1',
+    title:"Going To Mumbai",
+    body:"Hi Friends,I am going to Mumbai for vacations.Hope to enjoy a lot.Peace out",
+    reactions:2,
+    userId:"user-9",
+    tags:["vacation","Mumbai","Enjoying"],  
+},
+{
+    id:'2',
+    title:"Pass ho bhai!",
+    body:"4 saal ki masti k baad bhi ho gaye hai pass.Hard to believe",
+    reactions:15,
+    userId:"user-12",
+    tags:["Graduating","Unbelievable"],  
+},
+
+];
+
+
+
+export default PostListProvider;
